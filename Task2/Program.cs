ad44ddcf3c05d9426adda7d9dfdd842e2f08c1c7
Spiky_Coat <cat@test.ru>
2023-12-20 21:03:34 +0300

    rows replacement

---
+++ Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue) // Метод создает двумерный массив случайных чисел.
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeRows(int[,] inArray)
+{
+    int rowCount = inArray.GetLength(0) - 1;
+    for (int i = 0; i < inArray.GetLength(1); i++)
+    {
+        int k = inArray[0, i];
+        inArray[0, i] = inArray[rowCount, i];
+        inArray[rowCount, i] = k;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, columns, 0, 10);
+PrintArray(array);
+ChangeRows(array);
+Console.WriteLine("");
+PrintArray(array);
